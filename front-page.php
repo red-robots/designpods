148b1504017f00a47b0e2828eec4c16df0f50575
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-05-28 03:11:41 -0400

    Styling homepage

--- front-page.php
+++ front-page.php
@@ -39,10 +39,13 @@
 				<div class="col right animatedEl">
 					<div class="thinking wow fadeInRight" data-wow-duration="0.6s" data-wow-delay="0.8s">
 						<?php  
-							//include( locate_template('images/animated/me_thinking.svg') ); 
-							$thinkingImg = get_bloginfo("template_url") ."/images/animated/me_thinking.svg";
+							ob_start();
+							include( locate_template('images/animated/me_thinking.svg') ); 
+							//$thinkingImg = get_bloginfo("template_url") ."/images/animated/me_thinking.svg";
+							$thinking = ob_get_contents();
+							ob_end_clean();
+							echo $thinking;
 						?>
-							<img src="<?php echo $thinkingImg ?>" alt="">
 					</div>
 				</div>
 
