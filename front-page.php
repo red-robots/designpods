3b4d9d7a16a3366b5e7ce239092c3badbeed6b87
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-06-17 00:40:13 -0400

    Styled homepage for mobile view

--- front-page.php
+++ front-page.php
@@ -3,14 +3,20 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-<?php $row1Text = get_field("row1_text"); ?>
+<?php 
+$row1Text = get_field("row1_text"); 
+$row1TextMobile = get_field("row1_text_mobile"); 
+?>
 <div class="row-1 w100">
 	<div class="text">
 		<div class="wrapper">
 			<div class="titlediv fixed">
 				<div class="inline fadeIn animated">
 					<?php if ($row1Text) { ?>
-					<div id="row1-title"><?php echo $row1Text ?></div>
+					<div id="row1-title" class="row1-title"><?php echo $row1Text ?></div>
+					<?php } ?>
+					<?php if ($row1TextMobile) { ?>
+					<div id="row1-title-mobile" class="row1-title"><?php echo $row1TextMobile ?></div>
 					<?php } ?>
 				</div>
 			</div>
@@ -39,7 +45,11 @@
 			<div class="flexwrap">
 				
 				<div class="col left">
+
+					<div id="thinking-svg-mobile" class="animated fadeIn"></div>
+
 					<div class="wrap wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s">
+						
 						
 						<?php if ($row2_title) { ?>
 							<h2 class="h2"><?php echo $row2_title ?></h2>
@@ -58,14 +68,15 @@
 					</div>
 				</div>
 
-				<div class="col right animatedEl">
+				<?php $row2_svg = get_field("row2_svg"); ?>
+				<?php if ($row2_svg) { ?>
+				<div id="thinking-svg-desktop" class="col right animatedEl">
 					<div class="thinking wow fadeInRight" data-wow-duration="0.6s" data-wow-delay="0.8s">
-						<?php  
-							include( locate_template('images/svg/me_thinking.svg') ); 
-						?>
+						<img src="<?php echo $row2_svg['url'] ?>" alt="<?php echo $row2_svg['title'] ?>">
+						<?php  //include( locate_template('images/svg/me_thinking.svg') );  ?>
 					</div>
 				</div>
-
+				<?php } ?>
 			</div>
 		</div>
 	</div>
@@ -137,17 +148,14 @@ $featured_projects = get_field("featured_projects");
 		$row4Type = ($row4ImageType) ? $row4ImageType : '';
 		$row4_image = ($row4Type) ? get_field("row4_".$row4Type) : ""; 
 		$row4_group = get_field("row4_group"); 
+		$row4_svg = get_field("row4_svg"); 
 		?>
 		<div class="illustration-logo">
-			<span class="top-star"></span>
+			<span class="top-star wow zoomIn"></span>
 			<div class="flexwrap">
-				<?php if ($row4Type=='svg') { ?>
+				<?php if($row4_svg) { ?>
 				<div class="col graphic wow fadeIn" data-wow-delay="0.5s">
-					<div class="img-svg"><?php echo $row4_image ?></div>
-				</div>
-				<?php } else if($row4Type=='image') { ?>
-				<div class="col graphic wow fadeIn" data-wow-delay="0.5s">
-					<img src="<?php echo $row4_image['url'] ?>" alt="<?php echo $row4_image['title'] ?>" class="img-type">
+					<img src="<?php echo $row4_svg['url'] ?>" alt="<?php echo $row4_svg['title'] ?>" class="img-type">
 				</div>
 				<?php } ?>
 
@@ -191,6 +199,7 @@ $row5_image = ($row5Type) ? get_field("row5_".$row5Type) : "";
 $row5Clients = get_field("row5_clients");
 $row5Title = ( isset($row5Clients['heading']) && $row5Clients['heading'] ) ? $row5Clients['heading'] : '';
 $row5Brands = ( isset($row5Clients['brands']) && $row5Clients['brands'] ) ? $row5Clients['brands'] : '';
+$row5BrandsMobile = ( isset($row5Clients['brands_mobile']) && $row5Clients['brands_mobile'] ) ? $row5Clients['brands_mobile'] : '';
 ?>
 
 <div class="row-5 w100 row-brands">
@@ -216,10 +225,13 @@ $row5Brands = ( isset($row5Clients['brands']) && $row5Clients['brands'] ) ? $row
 							<h2 class="h2 font2"><?php echo $row5Title ?></h2>
 						<?php } ?>
 						
-						<?php if ($row5Brands) { ?>
+						<?php if ($row5Brands || $row5BrandsMobile) { ?>
 						<div class="clients">
 							<div class="logos">
-								<img class="all" src="<?php echo $row5Brands['url'] ?>" alt="<?php echo $row5Brands['title'] ?>">
+								<img class="all hidden-xs" src="<?php echo $row5Brands['url'] ?>" alt="<?php echo $row5Brands['title'] ?>">
+								<?php if ($row5BrandsMobile) { ?>
+								<img class="all visible-xs" src="<?php echo $row5BrandsMobile['url'] ?>" alt="<?php echo $row5BrandsMobile['title'] ?>">
+								<?php } ?>
 							</div>
 						</div>
 						<?php } ?>
@@ -244,12 +256,22 @@ $btn6Target =  (isset($row6_button['target']) && $row6_button['target']) ? $row6
 $row6ImageType = get_field("row6_image_type");
 $row6Type = ($row6ImageType) ? $row6ImageType : '';
 $row6_image = ($row6Type) ? get_field("row6_".$row6Type) : ""; 
+$row6_svg_image = get_field("row6_svg_image");
 
 if( $row6_title || $row6_text ) { ?>
 <div class="row-6 w100 abracadabra">
 	<div class="wrapper sm">
 		<div class="flexwrap">
 			<div class="skills">
+
+				<?php if ( $row6_svg_image ) { ?>
+				<div class="graphic visible-xs mobile">
+					<div class="graphic-svg wow fadeIn" data-wow-delay="0.5s">
+						<img src="<?php echo $row6_svg_image['url'] ?>" alt="<?php echo $row6_svg_image['title'] ?>" class="img-type">
+					</div>
+				</div>
+				<?php } ?>
+
 				<div class="wrap wow fadeIn">
 					<?php if ($row6_title) { ?>
 					<h2 class="t1 h2 font2"><?php echo $row6_title ?></h2>
@@ -270,17 +292,11 @@ if( $row6_title || $row6_text ) { ?>
 				
 			</div>
 
-			<?php if ( $row6_image && $row6Type ) { ?>
-			<div class="graphic">
-				<?php if ($row6Type=='svg') { ?>
+			<?php if ( $row6_svg_image ) { ?>
+			<div class="graphic hidden-xs desktop">
 				<div class="graphic-svg wow fadeInUp" data-wow-delay="0.5s">
-					<div class="img-svg"><?php echo $row6_image ?></div>
+					<img src="<?php echo $row6_svg_image['url'] ?>" alt="<?php echo $row6_svg_image['title'] ?>" class="img-type">
 				</div>
-				<?php } else if($row6Type=='image') { ?>
-				<div class="graphic-svg wow fadeInUp" data-wow-delay="0.5s">
-					<img src="<?php echo $row6_image['url'] ?>" alt="<?php echo $row6_image['title'] ?>" class="img-type">
-				</div>
-				<?php } ?>
 			</div>
 			<?php } ?>
 		</div>
@@ -290,5 +306,10 @@ if( $row6_title || $row6_text ) { ?>
 
 <?php endwhile; ?>
 
+<script>
+	if( $("#thinking-svg-desktop").length>0 ) {
+		$("#thinking-svg-mobile").html( $("#thinking-svg-desktop .thinking").html() );
+	}
+</script>
 <?php
 get_footer();
